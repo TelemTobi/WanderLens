dd96909c1794ef3c656b93d29d7b89e544017073
Telem Tobi <51072238+TelemTobi@users.noreply.github.com>
2023-09-17 22:32:32 +0300

    Initial Commit

---
+++ WanderLens/WanderLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  WanderLensApp.swift
+//  WanderLens
+//
+//  Created by Telem Tobi on 17/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WanderLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
